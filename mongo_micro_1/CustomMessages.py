8310f6ee7dcf4b13e171522a7ad7f30a5bd5e7d7
surajp <surajp@hifx.co.in>
2024-06-05 15:15:15 +0530

    Response structure standardised

---
+++ mongo_micro_1/CustomMessages.py
@@ -0,0 +1,8 @@
+class Custommessage(object):
+
+    def __init__(self) :
+        self.form_error = "Form contains error."
+        self.obj_not_found = "Requested data not found."
+        self.obj_create_success = "{obj} created successfully."
+        self.listed_successfully="Data listed successfully"
+        self.obj_deleted="Object deleted successfully"
--- mongo_micro_1/views.py
+++ mongo_micro_1/views.py
@@ -1,25 +1,33 @@
-from rest_framework import generics
+from rest_framework import generics,status
+from mongo_micro_1.CustomMessages import Custommessage
 from rest_framework.response import Response
 from .serializers import *
 from django.utils import timezone
 
 class CreateOrListSpendingObjAPIview(generics.GenericAPIView):
     serializer_class = CreateSpendingObjSerializer
+    msg_ob = Custommessage()
 
     def get(self,request,*args,**kwargs):
         data = SpendwiseBasicDetails.objects.all().order_by('-date')
         serialized_data = ListSpendingObjSerializer(data,many=True).data
-        return Response({"msg":"success","response":serialized_data})
+        return Response({"status":True, "msg": self.msg_ob.listed_successfully, 
+                "response":serialized_data},status=status.HTTP_200_OK) 
     
     def post(self,request):
         serializer = self.get_serializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({"msg":"success","response":"serialized_data"})
-        return Response({"Msg":serializer.errors})
+            return Response({"status":True, 
+                "msg": self.msg_ob.obj_create_success.format(obj="Spending"), 
+                "response":{}}, status=status.HTTP_200_OK)
+            
+        return Response({"status":False, "msg": self.msg_ob.obj_not_found, 
+                "response":{}}, status=status.HTTP_400_BAD_REQUEST) 
 
 class GetPerDayExpense(generics.GenericAPIView):   
     month = timezone.now().month
+    msg_ob = Custommessage()
     
     def generate_daily_expense(self,expenses):
         expense_dict = {
@@ -47,11 +55,14 @@ class GetPerDayExpense(generics.GenericAPIView):
             .order_by('date').values('date','reason','category','price')
             
         daily_expense = self.generate_daily_expense(expenses)
-        return Response({"expense_per_day":daily_expense})
+        return Response({"status":True, 
+                "msg": self.msg_ob.listed_successfully, 
+                "expense_per_day":daily_expense}, status=status.HTTP_200_OK)
 
 class GetCategoryWiseExpense(generics.GenericAPIView):
     serializer_class = CatExpensesSerializer
     month = timezone.now().month
+    msg_ob = Custommessage()
     
     def generate_category_chart(self,expenses):
         expense_dict = {
@@ -74,9 +85,13 @@ class GetCategoryWiseExpense(generics.GenericAPIView):
         cat_expenses_objs= SpendwiseBasicDetails.objects.filter(date__month=self.month).order_by('category')
         serialized_data = self.get_serializer(cat_expenses_objs,many=True)
         cat_expenses = self.generate_category_chart(serialized_data.data)
-        return Response({"category_wise":cat_expenses})
+        return Response({"status":True, 
+                    "msg": self.msg_ob.listed_successfully, 
+                    "category_wise":cat_expenses}, status=status.HTTP_200_OK)
 
 class DeleteSpendingObjAPIView(generics.GenericAPIView):
+    msg_ob = Custommessage()
+
     def get_object(self,spending_obj_id):
         try:
             return SpendwiseBasicDetails.objects.get(id=spending_obj_id)
@@ -86,6 +101,9 @@ class DeleteSpendingObjAPIView(generics.GenericAPIView):
     def delete(self,request,*args,**kargs):
         spend_obj = self.get_object(self.kwargs['spending_id'])
         if not spend_obj:
-            return Response({"response":'error'})
+            return Response({"status":True, "msg": self.msg_ob.obj_deleted, 
+                    "response":{}}, status=status.HTTP_200_OK)
         spend_obj.delete()
-        return Response({"Response":"success"})
+        return Response({"status":True, 
+                "msg": self.msg_ob.listed_successfully, 
+                "response":{}}, status=status.HTTP_200_OK)
